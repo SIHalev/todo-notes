6511102924719b2cc07dcd587a09ff38ef02b90a
Stoyan Halev <sto@foryouandyourcustomers.com>
2020-04-25 20:42:51 +0300

    Secret handling added

--- backend/src/logicLayer/emailSender.ts
+++ backend/src/logicLayer/emailSender.ts
@@ -9,29 +9,47 @@ import {createLogger} from "../utils/logger";
 
 const XAWS = AWSXRay.captureAWS(AWS);
 
+const secretManager = new AWS.SecretsManager();
+let cachedSecret: string; // Only for testing reuse. For PRD it will not be stored, because we invoke once a day
+
 const SES = new XAWS.SES({region: 'us-east-1'});
 
 const logger = createLogger('emailSender');
 
-const SEND_GRID_API_KEY = process.env.SEND_GRID_API_KEY;
+const secretId = process.env.SEND_GRID_SECRET_ID;
+const secretField = process.env.SEND_GRID_SECRET_FIELD;
 
 export async function sendEmail(email: EmailMessage) {
     logger.info(`Sending email ${email}`);
     // The education sandbox account didn't have enough permissions for aws ses, so I am using external service
-    // return await sendEmailSendGrid(email);
     return await sendEmailSendGrid(email);
 }
 
+// For testing purposes
 export async function sendEmailFake(email: EmailMessage) {
     logger.info(email);
 }
 
 export async function sendEmailSendGrid(email: EmailMessage) {
-    logger.info(SEND_GRID_API_KEY);
     logger.info(email);
-    sgMail.setApiKey(SEND_GRID_API_KEY);
+    const secretObject: any = await getSecret();
+    const secretKey = secretObject[secretField];
+    sgMail.setApiKey(secretKey);
     const response: ClientResponse = await sgMail.send(email);
     logger.info(response);
+}
+
+async function getSecret() {
+    if (!cachedSecret) {
+        logger.info("Fetching send grid secret key");
+        const data = await secretManager.getSecretValue({
+            SecretId: secretId
+        }).promise();
+
+        cachedSecret = JSON.parse(data.SecretString);
+    }
+
+    return cachedSecret;
 }
 
 export async function sendEmailSES(email: EmailMessage) {
